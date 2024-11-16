a10721712d403f0244ac75866e8a942ae5b201b5
Shubham7476 <shubhamsonake307@gmail.com>
2024-11-17 00:33:24 +0530

    theme switching

--- frontend/src/components/public/Navbar.jsx
+++ frontend/src/components/public/Navbar.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
 import { Button } from '../ui/button';
-import { LogOut, User2 } from 'lucide-react';
+import { LogOut, User2, Sun, Moon } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+    const [theme, setTheme] = useState('light');
     const user = false;
 
+    useEffect(() => {
+        document.documentElement.className = theme;
+    }, [theme]);
+
+    const toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+    };
+
     return (
-        <div className="navbar bg-white flex items-center">
+        <div className={`navbar ${theme === 'light' ? 'bg-white text-black' : 'bg-gray-800 text-white'} flex items-center justify-between`}>
             {/* Navbar Start */}
             <div className="navbar-start">
-                {/* Mobile Dropdown Button */}
                 <div className="dropdown lg:hidden">
                     <label tabIndex={0} role="button" className="btn btn-ghost">
                         <svg
@@ -27,7 +35,7 @@ function Navbar() {
                     </label>
                     <ul
                         tabIndex={0}
-                        className="menu menu-sm dropdown-content mt-3 w-52 p-2 bg-white rounded-box"
+                        className={`menu menu-sm dropdown-content mt-3 w-52 p-2 ${theme === 'light' ? 'bg-white text-black' : 'bg-gray-800 text-white'} rounded-box`}
                     >
                         <li><a>Home</a></li>
                         <li><a>Jobs</a></li>
@@ -35,7 +43,7 @@ function Navbar() {
                         {!user ? (
                             <>
                                 <Link to="/signup"><Button className="border-2 bg-blue-600 rounded-md">Signup</Button></Link>
-                                <Link to="/login" ><Button className="border-2 bg-blue-600 hover:bg-yellow-500">Login</Button></Link>
+                                <Link to="/login"><Button className="border-2 bg-blue-600 hover:bg-yellow-500">Login</Button></Link>
                             </>
                         ) : (
                             <Popover>
@@ -45,7 +53,7 @@ function Navbar() {
                                         <AvatarFallback>CN</AvatarFallback>
                                     </Avatar>
                                 </PopoverTrigger>
-                                <PopoverContent className="w-72 bg-white">
+                                <PopoverContent className={`w-72 ${theme === 'light' ? 'bg-white text-black' : 'bg-gray-800 text-white'}`}>
                                     <div className="flex gap-3 mb-2">
                                         <Avatar>
                                             <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
@@ -53,17 +61,17 @@ function Navbar() {
                                         </Avatar>
                                         <div>
                                             <h4>Shubham Sonake</h4>
-                                            <p className="text-sm text-muted-foreground">Lorem ipsum dolor sit amet.</p>
+                                            <p className={`text-sm ${theme === 'light' ? 'text-black' : 'text-white'}`}>Lorem ipsum dolor sit amet.</p>
                                         </div>
                                     </div>
                                     <div className="flex flex-col items-start my-0.5">
                                         <div className="flex items-center cursor-pointer">
                                             <User2 />
-                                            <Button variant="link">View Profile</Button>
+                                            <Button variant="link" className={`text-sm ${theme === 'light' ? 'text-black' : 'text-white'}`}>View Profile</Button>
                                         </div>
                                         <div className="flex items-center cursor-pointer">
                                             <LogOut />
-                                            <Button variant="link">Logout</Button>
+                                            <Button variant="link" className={`text-sm ${theme === 'light' ? 'text-black' : 'text-white'}`}>Logout</Button>
                                         </div>
                                     </div>
                                 </PopoverContent>
@@ -71,22 +79,28 @@ function Navbar() {
                         )}
                     </ul>
                 </div>
-
-                {/* Logo */}
-                <a className="btn btn-ghost text-xl"><Link to='/'>Talentry</Link></a>
+                <a className={`btn btn-ghost text-xl ${theme === 'light' ? 'text-black' : 'text-white'}`}>
+                    <Link to="/">Talentry</Link>
+                </a>
             </div>
 
             {/* Navbar End for Larger Screens */}
-            <div className="navbar-end flex items-center gap-4">
-                <ul className="menu menu-horizontal hidden lg:flex px-1">
+            <div className="navbar-center flex-grow hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 space-x-4">
                     <li><a>Home</a></li>
                     <li><a>Jobs</a></li>
                     <li><a>Browse</a></li>
                 </ul>
+            </div>
+
+            <div className="navbar-end flex items-center gap-4">
+                <button onClick={toggleTheme} className="btn btn-ghost">
+                    {theme === 'light' ? <Moon className="w-5 h-5" /> : <Sun className="w-5 h-5" />}
+                </button>
                 {!user ? (
                     <div className="flex items-center gap-3">
                         <Link to="/signup"><Button className="border-2 bg-blue-600 rounded-md">Signup</Button></Link>
-                        <Link to="/login" ><Button className="border-2 bg-blue-600 hover:bg-yellow-500">Login</Button></Link>
+                        <Link to="/login"><Button className="border-2 bg-blue-600 hover:bg-yellow-500">Login</Button></Link>
                     </div>
                 ) : (
                     <Popover>
@@ -96,7 +110,7 @@ function Navbar() {
                                 <AvatarFallback>CN</AvatarFallback>
                             </Avatar>
                         </PopoverTrigger>
-                        <PopoverContent className="w-72 bg-white">
+                        <PopoverContent className={`w-72 ${theme === 'light' ? 'bg-white text-black' : 'bg-gray-800 text-white'}`}>
                             <div className="flex gap-3 mb-2">
                                 <Avatar>
                                     <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
@@ -104,17 +118,17 @@ function Navbar() {
                                 </Avatar>
                                 <div>
                                     <h4>Shubham Sonake</h4>
-                                    <p className="text-sm text-muted-foreground">Lorem ipsum dolor sit amet.</p>
+                                    <p className={`text-sm ${theme === 'light' ? 'text-black' : 'text-white'}`}>Lorem ipsum dolor sit amet.</p>
                                 </div>
                             </div>
                             <div className="flex flex-col items-start my-0.5">
                                 <div className="flex items-center cursor-pointer">
                                     <User2 />
-                                    <Button variant="link">View Profile</Button>
+                                    <Button variant="link" className={`text-sm ${theme === 'light' ? 'text-black' : 'text-white'}`}>View Profile</Button>
                                 </div>
                                 <div className="flex items-center cursor-pointer">
                                     <LogOut />
-                                    <Button variant="link">Logout</Button>
+                                    <Button variant="link" className={`text-sm ${theme === 'light' ? 'text-black' : 'text-white'}`}>Logout</Button>
                                 </div>
                             </div>
                         </PopoverContent>
